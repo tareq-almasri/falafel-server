961f6930619bf37aa3bf2a6e83c557a5b7450243
tareq-almasri <talmasreq@gmail.com>
2020-08-29 17:31:39 +0200

    sign up form updated to only register user when info next btn is pressed

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ mongoose.connect(
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   },
-  err => {
+  (err) => {
     if (!err) {
       console.log("MongoDB Connection Succeeded.");
     } else {
@@ -25,7 +25,7 @@ mongoose.connect(
   }
 );
 
-let port=5000;
+let port = 5000;
 app.listen(port, () => {
   console.log("start listening on port 5000");
 });
@@ -41,104 +41,89 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post("/sign-up", (req, res) => {
-  let newUser = req.body.username;
-  let pwd = bcrypt.hashSync(req.body.password, 10);
-
-  User.findOne({ username: newUser })
-    .then(user => {
-      if (!user) {
-        User.create({
-          username: newUser,
-          password: pwd
-        }).then(userNew => {
-          return res.send(userNew);
-        });
-      }
-      // user already exists!
-      else {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        // user already exists!
         res.status(400).send({
-          error: "username already exists"
+          error: "username already exists",
         });
       }
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
-app.post('/info', (req,res)=>{
-    
-    User.findOne({ username: req.body.username })
-      .then(user => {
-        user.tdee = req.body.tdee;
-        user.goalCal = req.body.goal;
-        user.diet = req.body.diet;
-        user.proteinDL = req.body.protein;
-        user.carbsDL = req.body.carbs;
-        user.fatDL = req.body.fat;
-        user.sugarDL = req.body.sugar;
-        user.caffDL = 400;
-        user.save();
-        res.json("info saved");
-      })
-      .catch(err => res.status(400).json("Error: " + err));
-})
+app.post("/info", (req, res) => {
+  let pwd = bcrypt.hashSync(req.body.password, 10);
+  User.create({
+    username: req.body.username,
+    password: pwd,
+    tdee : req.body.tdee,
+    goalCal : req.body.goal,
+    diet : req.body.diet,
+    proteinDL : req.body.protein,
+    carbsDL : req.body.carbs,
+    fatDL : req.body.fat,
+    sugarDL : req.body.sugar,
+    caffDL : 400
+  })
+    .then((user) => {
+     
+      return res.send(user);
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
 app.post("/set-plan", (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
-      user.dailyPlan=req.body.dailyPlan;
-      user.sport=req.body.sport;
+    .then((user) => {
+      user.dailyPlan = req.body.dailyPlan;
+      user.sport = req.body.sport;
       user.save();
       res.json("plan saved");
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 app.post("/login", (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
+    .then((user) => {
       // user with this email not found? => error
       if (!user) {
         res.status(400).send({ err: "invalid username or password" });
       } else {
         // compare passwords using bcrypt.compare() function
-        bcrypt.compare(req.body.password, user.password).then(success => {
+        bcrypt.compare(req.body.password, user.password).then((success) => {
           // user password does not match password from login form? => error
           if (!success) {
             res.status(400).send({ err: "invalid username or password" });
           } else {
             // create JWT token by signing
             // let secret = 'secret';
-            // let token = jwt.sign(
-            //   { username: user.username, aud: "iPhone-App" },
-            //   secret
-            // );
-
-            // ,{ expiresIn: "1h" }
-
-            // return token
-            res.send({ token: user.username }); // => same as: { "token": token }
+            // let token = jwt.sign({username: user.username, aud: "iPhone-App"}, secret, {expiresIn: "1h"});
+            res.send({ usernameB: user.username}); // => same as: { "token": token }
           }
         });
       }
     })
-    .catch(err => res.status(400).json("err: " + err));
+    .catch((err) => res.status(400).json("err: " + err));
 });
 
 app.post("/home", (req, res) => {
-  User.findOne({username: req.body.username})
-    .then(userFound => {
+  User.findOne({ username: req.body.username })
+    .then((userFound) => {
       if (!userFound) {
         res.send({ err: "user not found" });
       } else {
         res.send({ found: userFound });
       }
     })
-    .catch(err => res.status(400).json("err: " + err));
+    .catch((err) => res.status(400).json("err: " + err));
 });
 
 app.post("/reset", (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
+    .then((user) => {
       user.udi = {
         date: req.body.date,
         calCount: 0,
@@ -147,18 +132,17 @@ app.post("/reset", (req, res) => {
         carbsCount: 0,
         waterCount: 0,
         caffCount: 0,
-        sugarCount: 0
+        sugarCount: 0,
       };
       user.save();
       res.json("udi reset Succeeded");
     })
-    .catch(err => res.status(400).json("Error: " + err));
-
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 app.post("/add", (req, res) => {
   User.findOne({ username: req.body.username })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         res.send({ err: "user not found" });
       } else {
@@ -167,5 +151,5 @@ app.post("/add", (req, res) => {
         res.json("udi updated");
       }
     })
-    .catch(err => res.status(400).json("err: " + err));
+    .catch((err) => res.status(400).json("err: " + err));
 });
